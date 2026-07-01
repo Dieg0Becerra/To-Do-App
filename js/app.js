d1e286ee2949d101268405155de91b41c3641663
Diego <becerra.diego9@gmail.com>
2026-06-30 21:25:08 -0700

    added basic todo functionality with clicking

--- js/app.js
+++ js/app.js
@@ -2,3 +2,19 @@ const btn = document.getElementById("add-btn")
 const input = document.getElementById("todo-input")
 const list = document.getElementById("list")
 
+
+btn.addEventListener('click', () => addTodo())
+
+
+
+function addTodo()
+{    
+    const value = input.value
+
+    if (value == "") return
+
+    const item = document.createElement('li')
+    item.innerText = value
+    list.appendChild(item)
+    input.value = ""
+}
