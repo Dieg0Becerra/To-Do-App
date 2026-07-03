70a11270778598a8fc0fe51c33c51f0491a35239
Diego <becerra.diego9@gmail.com>
2026-07-03 16:18:18 -0700

    enter key functionality

---
+++ README.md
@@ -0,0 +1,9 @@
+Planned Features
+1. ~~Complete/checkbox~~
+2. localStorage
+3. Long/short term categories - add done attribute
+4. Priority + due dates + auto escalation
+5. Sorting
+6. Firebase database
+7. Auth
+8. CSS
--- js/app.js
+++ js/app.js
@@ -1,26 +1,55 @@
 const btn = document.getElementById("add-btn")
 const input = document.getElementById("todo-input")
 const list = document.getElementById("list")
+const completedList = document.getElementById("completed-list")
+
+const todo = {}
+
 
 
 btn.addEventListener('click', () => addTodo())
-
+input.addEventListener('keypress', (e) =>
+    {
+        if (e.key === "Enter")
+        {
+            addTodo()
+        }
+    })
 
 
 function addTodo()
 {    
+    //the basic functionality to add tasks
     const value = input.value
 
-    if (value == "") return
+    if (value === "") return
 
     const item = document.createElement('li')
     item.innerText = value
 
+    list.append(item)
+    input.value = "" //clears input
+
+
+
+    //delete functionality
     const deleteBtn = document.createElement("button")
+    deleteBtn.innerText = "delete"
     item.append(deleteBtn)
 
     deleteBtn.addEventListener("click", () => {item.remove()})
 
-    list.append(item)
-    input.value = ""
+
+
+    //complete functionaility
+    const completeBtn = document.createElement("button")
+    completeBtn.innerText = "complete"
+    item.append(completeBtn)
+
+    completeBtn.addEventListener("click", () => 
+        {
+            completedList.append(item)
+            completeBtn.remove()
+        })
+
 }
